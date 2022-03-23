945e1c9abc2ff1a94f742f957d77507f56b9a330
nirodriguezp <nirodriguezp@unal.edu.co>
2022-03-23 00:47:14 -0500

    Comentarios agregados

--- subs_cancel.cpp
+++ subs_cancel.cpp
@@ -2,20 +2,20 @@
 #include <cmath>
 #include <iomanip>
 
-float x1_1(float c);
-float x1_2(float c);
-float x2_1(float c);
-float x2_2(float c);
+float x1_1(float c); //forma 1 de solucionar la ec. cuadratica, solucion 1
+float x1_2(float c); //forma 1 de solucionar la ec. cuadratica, solucion 2
+float x2_1(float c); //forma 2 de solucionar la ec. cuadratica, solucion 1
+float x2_2(float c); //forma 2 de solucionar la ec. cuadratica, solucion 2
 float a=1.0, b=1.0;
 
 int main(int argc, char **argv){
 
     std::cout << "\t" << "c" << "\t\t" << "x_1" << "\t\t" << "x_2" << "\t\t" << "x'_1" << "\t\t" << "x'_2" << "\n";
 
-    std::setprecision(7);
+    std::setprecision(7); //7 decimales 
     for(int n=1; n<=10; n++){
 
-      float c = std::pow(10, -n);
+      float c = std::pow(10.0, -n); 
       std::cout << std::scientific << c << "\t" << x1_1(c) << "\t" << x1_2(c) << "\t" << x2_1(c) << "\t" << x2_2(c) << "\n";
 
     }      
