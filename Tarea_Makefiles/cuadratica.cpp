c9990d7a52820ff2718c5f257664ffabcb45b6c6
nirodriguezp <nirodriguezp@unal.edu.co>
2022-04-01 02:29:05 -0500

    Ultima version

--- Tarea_Makefiles/cuadratica.cpp
+++ Tarea_Makefiles/cuadratica.cpp
@@ -10,13 +10,11 @@ double x2_2(double a, double b, double c); //forma 2 de solucionar la ec. cuadra
 int main(int argc, char **argv){
     
     double a=1.0, b=1.0;
-    std::cout << "\t" << "c" << "\t\t" << "x_1" << "\t\t" << "x_2" << "\t\t" << "x'_1" << "\t\t" << "x'_2" << "\n";
-
-    std::setprecision(15);  
     for(int n=1; n<=10; n++){
 
-      double c = std::pow(10.0, -n); 
-      std::cout << std::scientific << c << "\t" << x1_1(a, b, c) << "\t" << x1_2(a, b, c) << "\t" << x2_1(a, b, c) << "\t" << x2_2(a, b, c) << "\n";
+      double c = std::pow(10.0, -n);
+
+      std::cout << std::scientific << n << "\t" << c << "\t" << x1_1(a, b, c) << "\t" << x1_2(a, b, c) << "\t" << x2_1(a, b, c) << "\t" << x2_2(a, b, c) << "\t"<< std::fabs(x1_1(a, b, c)-x2_1(a, b, c))/x2_1(a, b, c) << "\t" << std::fabs(x1_2(a, b, c)-x2_2(a, b, c))/x2_2(a, b, c) << "\n";
 
     }      
 }
